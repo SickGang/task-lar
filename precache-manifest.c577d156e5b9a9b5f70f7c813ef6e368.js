43948d0a5c3d143f6afa6c68d74fb48984105a54
Maksim <max.ru6@mail.ru>
2021-02-21 17:48:17 +0400

    change vue.config.js

---
+++ precache-manifest.c577d156e5b9a9b5f70f7c813ef6e368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3733cf8b5b19593d9d7d",
+    "url": "/task-lar/css/app.86dfb72d.css"
+  },
+  {
+    "revision": "82890314b26dd64b3b0e",
+    "url": "/task-lar/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "be3dd139a1442b6fb14eee2aabd4d01b",
+    "url": "/task-lar/index.html"
+  },
+  {
+    "revision": "3733cf8b5b19593d9d7d",
+    "url": "/task-lar/js/app.8e9aaa31.js"
+  },
+  {
+    "revision": "82890314b26dd64b3b0e",
+    "url": "/task-lar/js/chunk-vendors.1311608a.js"
+  },
+  {
+    "revision": "c6e71a059b7bac3556ffdbdf57933501",
+    "url": "/task-lar/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/task-lar/robots.txt"
+  }
+]);
